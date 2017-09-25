9d5b6b4ef1851569cd15413d42858a4c2e5e7bd1
Rasch <cxr0002@dcccd.edu>
2017-09-25 15:06:56 -0500

    init

---
+++ src/Mail.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const nodemailer = require('nodemailer');
+
+class Mail extends React.Component {
+    render() {
+        return (
+        
+        );
+    }
+}
+
+export default Mail;
